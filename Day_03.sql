ebd27ef716cf68568564028bf2c54b0e698f575e
Sourav749 <sourav199sahoo@gmail.com>
2025-11-05 22:47:18 +0530

    Add Day 03 - Sorting Data with ORDER BY

---
+++ Day_03.sql
@@ -0,0 +1,13 @@
+/*  Retrieve the top 5 weeks with the highest patient refusals across all services, showing week, 
+service, patients_refused, and patients_request. Sort by patients_refused in descending order. */
+
+select 
+  week, 
+  service, 
+  patients_refused, 
+  patients_request 
+from 
+  services_weekly 
+order by 
+  patients_refused desc 
+limit 5;
