b9fac2da9528e104199344f3476877027ea44397
Sourav749 <sourav199sahoo@gmail.com>
2025-11-08 20:41:36 +0530

    Add Day 05 - Aggregate Functions

--- Day_05.sql
+++ Day_05.sql
@@ -1,3 +1,12 @@
 /* Calculate the total number of patients admitted, total patients refused, 
 and the average patient satisfaction across all services and weeks. 
 Round the average satisfaction to 2 decimal places. */
+
+select 
+  sum(patients_admitted) AS Total_Patients_Admitted, 
+  sum(patients_refused) AS Total_Patients_Refused, 
+  round(
+    Avg(patient_satisfaction), 
+    2) AS Avg_Patient_Satisfaction 
+from 
+  services_weekly;
