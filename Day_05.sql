78832e73af07d8fb31408c825ff61a49076717ee
Sourav749 <sourav199sahoo@gmail.com>
2025-11-08 08:16:32 +0530

    Add Day 05 - Aggregate Functions

---
+++ Day_05.sql
@@ -0,0 +1,3 @@
+/* Calculate the total number of patients admitted, total patients refused, 
+and the average patient satisfaction across all services and weeks. 
+Round the average satisfaction to 2 decimal places. */
